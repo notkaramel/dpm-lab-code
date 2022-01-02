5994e30e7bb609749575dd1f2013dd23b92db924
Ryan Au <auryan898@gmail.com>
2022-01-02 06:12:30 -0500

    Improve telemtry, fix missing label bug on first add

--- utils/telemetry.py
+++ utils/telemetry.py
@@ -8,6 +8,7 @@ _EXIT_FLAG = True
 
 
 def _on_closing():
+    """Private method: cleans up internal values on window destruction"""
     global WINDOW, _EXIT_FLAG, LABELS
     _EXIT_FLAG = True
     WINDOW.destroy()
@@ -16,51 +17,60 @@ def _on_closing():
 
 
 def start():
+    """Open the telemetry window"""
     global WINDOW, _EXIT_FLAG
     _EXIT_FLAG = False
     if WINDOW is None:
         WINDOW = tk.Tk()
-    WINDOW.protocol("WM_DELETE_WINDOW", _on_closing)
+    else:
+        WINDOW.protocol("WM_DELETE_WINDOW", _on_closing)
     update()
 
 
 def isopen():
+    """Determines if the telemtry window has been opened or closed"""
     return not _EXIT_FLAG
 
 
 def resize(width, height):
+    """Resize telemtry to a set width and height in pixels"""
     if WINDOW is None:
         return
     WINDOW.geometry("{}x{}".format(width, height))
 
 
 def stop():
+    """Closes window"""
     global WINDOW
     if WINDOW is not None:
         WINDOW.quit()
         WINDOW = None
 
 
-def add(key, data, showboth=False):
+def add(key, data, showkey=False):
+    """Adds/Sets data by a key to the telemetry window"""
     if WINDOW is None:
         return
     key = str(key)
     data = str(data)
-    if showboth:
+    if showkey:
         data = "{} : {}".format(key, data)
     if key in LABELS:
         LABELS[key][1].set(data)
     else:
         var = StringVar()
+        var.set(data)
         LABELS[key] = (tk.Label(WINDOW, textvariable=var), var)
         LABELS[key][0].pack()
 
 
-def update():
+def update(retries=1):
+    """Updates display with latest telemetry values"""
     global WINDOW
     if WINDOW is not None:
         try:
-            WINDOW.update()
+            for i in range(retries):
+                WINDOW.update()
         except TclError as e:
             err = str(e)
             if err == 'can\'t invoke "update" command: application has been destroyed':
@@ -68,6 +78,7 @@ def update():
 
 
 def clear():
+    """Destroy and remove all LABELS of telemetry"""
     global LABELS
     for i, widget in LABELS.items():
         widget[0].destroy()
@@ -79,16 +90,23 @@ if __name__ == '__main__':
     start()
     resize(500, 200)
     i = 0
+    add("word","heyo this is the start")
+    update()
     while True:
-        time.sleep(0.5)
+        time.sleep(1)
+
+        ### Test clearing window despite still updating ###
         if i == 10:
             clear()
             if not isopen():
                 start()
 
-        add("color", "red")
+        ### Adding data
+        add("color", "red", True)
         i = i + 2 if i < 40 else 0
 
         print(i, isopen())
         add("counter", "*"*i)
+
+        ### Must update window to see changes
         update()
