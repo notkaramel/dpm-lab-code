8e80f3a19ebc9542942457aa3bcb85d5e2b32575
Ryan Au <auryan898@gmail.com>
2022-01-13 16:35:49 -0500

    Add comments to lecture examples to complete them

--- lecture_2_1_ColorData.py
+++ lecture_2_1_ColorData.py
@@ -1,38 +1,37 @@
 from utils.brick import BP, Motor, TouchSensor, EV3ColorSensor, wait_ready_sensors, SensorError
 import time
 
-FORWARD_SPEED = 30
-SENSOR_POLL_SLEEP = 0.05
+FORWARD_SPEED = 30           # speed constant = 30% power
+SENSOR_POLL_SLEEP = 0.05     # Polling rate = 50 msec
 
-T_SENSOR = TouchSensor(1)
-C_SENSOR = EV3ColorSensor(2)
-
-LEFT_MOTOR = Motor("A")
-RIGHT_MOTOR = Motor("D")
+T_SENSOR = TouchSensor(1)    # Touch Sensor in Port S1
+C_SENSOR = EV3ColorSensor(2) # Color Sensor in Port S2
 
 try:
-    wait_ready_sensors()  # Initialize sensors
+    f = open("colorSensor.csv", "w")              # Open a file for writing
+    f.write("Data logged from color sensor.\n")   # Print a header
+
+    print('Waiting for sensors to initialize...')
+    wait_ready_sensors()                          # Wait for sensors to initialize
+    print("Data acquisition commencing...")
     
-    f = open("colorSensor.csv", "w")
-    f.write("Data logged from color sensor.\n")
-    print("Data acquisition commencing.")
-
-    LEFT_MOTOR.set_power(FORWARD_SPEED)
-    RIGHT_MOTOR.set_power(FORWARD_SPEED)
-
     while True:
         try:
-            if T_SENSOR.is_pressed():
-                print("Data acquisition complete.")
-                f.close()
-                BP.reset_all()
-            red, gre, blu, lum = C_SENSOR.get_value()
-            f.write('{:d},{:d},{:d},{:d}\n'.format(red, gre, blu, lum))
-            time.sleep(SENSOR_POLL_SLEEP)
-        except SensorError as error:
-            print(error)
+            if T_SENSOR.is_pressed():             # End data acquisition if sensor pressed
+                print("Data acquisition complete. Closing log file.")
+                f.close()                         # Close file (save contents)
+                BP.reset_all()                    # Stop robot
 
-except KeyboardInterrupt:
-    print("Data acquisition interrupted.")
-    f.close()
+            # Retrieve the color values the Color sensor
+            red, gre, blu, lum = C_SENSOR.get_value()
+
+            # Write color values to the file
+            f.write('{:d},{:d},{:d},{:d}\n'.format(red, gre, blu, lum))
+
+            time.sleep(SENSOR_POLL_SLEEP)         # Use sensor polling interval here
+        except SensorError as error:
+            print(error)                          # On exception or error, print error code
+
+except KeyboardInterrupt:                         # Allows program to be stopped on keyboard interrupt
+    print("Data acquisition aborted.")
     BP.reset_all()
--- lecture_2_2_MedianFilter.py
+++ lecture_2_2_MedianFilter.py
@@ -1,21 +1,31 @@
 import math
 
-def median(arr):
-    N = len(arr) # length of array
-    arr = sorted(arr)
-    if N%2==1:
-        return arr[ math.ceil(N/2) ]
-    else:
-        return (arr[N//2] + arr[N//2 + 1]) / 2
+def median_filter(samples, width):
+    """width must be an odd number"""
+    N = len(samples)
+    
+    result = []
+    for i in range(0, N-width):
+        sv = sorted(samples[i:i+width])
+        med_i = math.floor(width/2)
+        result.append(sv[med_i])
+    
+    return result
+
+print(median_filter([1,2,3,4,5,6,7,8,9,10,11], 3))
+
+from statistics import median
 
 def median_filter(samples, width):
     N = len(samples)
     
     result = []
-    for i in range(0, N, width):
+    for i in range(0, N-width):
         m = median(samples[i:i+width])
-        for j in range(min(width, N-i)):
-            result.append(m)
+        result.append(m)
     
     return result
-        
+
+print(median_filter([1,2,3,4,5,6,7,8,9,10,11], 3))
+
+
