f25e43a70f8db364f1444efae5815d01a8b300b5
Ryan Au <auryan898@gmail.com>
2022-01-13 15:50:17 -0500

    Added comments to complete this code example

--- lecture_1_1_Walkthrough.py
+++ lecture_1_1_Walkthrough.py
@@ -1,26 +1,25 @@
 from utils.brick import BP, TouchSensor, Motor, wait_ready_sensors, SensorError
 import time
 
-FORWARD_SPEED = 30  # 30% power
-SENSOR_POLL_SLEEP = 0.05
+FORWARD_SPEED = 30          # speed constant = 30% power
+SENSOR_POLL_SLEEP = 0.05    # Polling rate = 50 msec
 
-T_SENSOR = TouchSensor(1)
-LEFT_MOTOR = Motor("A")
-RIGHT_MOTOR = Motor("D")
+T_SENSOR = TouchSensor(1)   # Touch Sensor in Port S1
+LEFT_MOTOR = Motor("A")     # Left motor in Port A
+RIGHT_MOTOR = Motor("D")    # Right motor in Port D
 
 try:
-    wait_ready_sensors()  # Initialize sensors
-    LEFT_MOTOR.set_power(30)
-    RIGHT_MOTOR.set_power(30)
+    wait_ready_sensors()                  # Wait for sensors to initialize
+    LEFT_MOTOR.set_power(FORWARD_SPEED)   # Start left motor
+    RIGHT_MOTOR.set_power(FORWARD_SPEED)  # Simultaneously start right motor
 
     while True:
         try:
-            if T_SENSOR.is_pressed():
+            if T_SENSOR.is_pressed():      # Press touch sensor to stop robot
                 BP.reset_all()
-                exit()
-            time.sleep(SENSOR_POLL_SLEEP)
+            time.sleep(SENSOR_POLL_SLEEP)  # Use sensor polling interval here
         except SensorError as error:
-            print(error)
+            print(error)                   # On exception or error, print error code
 
-except KeyboardInterrupt:
+except KeyboardInterrupt:                  # Allows program to be stopped on keyboard interrupt
     BP.reset_all()
--- lecture_1_2_Walkthrough.py
+++ lecture_1_2_Walkthrough.py
@@ -1,34 +1,38 @@
 from utils.brick import BP, Motor, TouchSensor, EV3ColorSensor, wait_ready_sensors, SensorError
 import time
 
-FORWARD_SPEED = 30
-SENSOR_POLL_SLEEP = 0.05
+FORWARD_SPEED = 30           # speed constant = 30% power
+SENSOR_POLL_SLEEP = 0.05     # Polling rate = 50 msec
 
-T_SENSOR = TouchSensor(1)
-C_SENSOR = EV3ColorSensor(2)
+T_SENSOR = TouchSensor(1)    # Touch Sensor in Port S1
+C_SENSOR = EV3ColorSensor(2) # Color Sensor in Port S2
 
-LEFT_MOTOR = Motor("A")
-RIGHT_MOTOR = Motor("D")
+LEFT_MOTOR = Motor("A")      # Left motor in Port A
+RIGHT_MOTOR = Motor("D")     # Right motor in Port D
 
 try:
-    f = open("colorSensor.txt", "w+")
-    f.write("Data logged from color sensor.\n")
+    f = open("colorSensor.txt", "w+")             # Open a file for writing
+    f.write("Data logged from color sensor.\n")   # Print a header
 
-    wait_ready_sensors()  # Initialize sensors
-
-    LEFT_MOTOR.set_power(FORWARD_SPEED)
-    RIGHT_MOTOR.set_power(FORWARD_SPEED)
+    wait_ready_sensors()                  # Wait for sensors to initialize
+    LEFT_MOTOR.set_power(FORWARD_SPEED)   # Start left motor
+    RIGHT_MOTOR.set_power(FORWARD_SPEED)  # Simultaneously start right motor
 
     while True:
         try:
-            if T_SENSOR.is_pressed():
+            if T_SENSOR.is_pressed():     # Press touch sensor to stop robot
                 BP.reset_all()
-            red, gre, blu, lum = C_SENSOR.get_value()
-            f.write('R={:d},G={:d},B={:d},L={:d}\n'.format(red, gre, blu, lum))
-            time.sleep(SENSOR_POLL_SLEEP)
-        except SensorError as error:
-            print(error)
 
-except KeyboardInterrupt:
+            # Retrieve the color values the Color sensor
+            red, gre, blu, lum = C_SENSOR.get_value()
+
+            # Write color values to the file
+            f.write('R={:d},G={:d},B={:d},L={:d}\n'.format(red, gre, blu, lum))
+            
+            time.sleep(SENSOR_POLL_SLEEP)  # Use sensor polling interval here
+        except SensorError as error:
+            print(error)                   # On exception or error, print error code
+
+except KeyboardInterrupt:                  # Allows program to be stopped on keyboard interrupt
     f.close()
     BP.reset_all()
