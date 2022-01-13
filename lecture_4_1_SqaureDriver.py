93041eae61085656edc0ab232e270e5fcf3ae761
Ryan Au <auryan898@gmail.com>
2022-01-13 16:57:55 -0500

    Add comments to complete square driver example

--- lecture_4_1_SqaureDriver.py
+++ lecture_4_1_SqaureDriver.py
@@ -1,79 +1,93 @@
-import time, math
+import time
+import math
 from utils import brick
 from utils.brick import BP, Motor
 
 MOTOR_POLL_DELAY = 0.05
 
-SQUARE_LENGTH = 0.5 # meters
-WHEEL_RADIUS = 0.028 # meters
-AXEL_LENGTH = 0.09 # meters
+SQUARE_LENGTH = 0.5     # (meters) Side length of square in
+WHEEL_RADIUS = 0.028    # (meters) Radius of one wheel
+AXLE_LENGTH = 0.09      # (meters) Distance between wheel contact with ground
 
-DIST_TO_DEG = 180/(math.pi*WHEEL_RADIUS) # degrees / meter
-ORIENT_TO_DEG = AXEL_LENGTH / WHEEL_RADIUS
+DIST_TO_DEG = 180/(math.pi*WHEEL_RADIUS)   # (degrees / meter) Convert distance to wheel degrees
+ORIENT_TO_DEG = AXLE_LENGTH / WHEEL_RADIUS # Convert whole robot rotation to wheel rotation
 
-FWD_SPEED = 180 # deg per sec
-TRN_SPEED = 90  # dps
+FWD_SPEED = 180  # (deg per sec) Moving forward speed
+TRN_SPEED = 90   # (deg per sec) Turning a corner speed
 
-LEFT_MOTOR = Motor("A")
-RIGHT_MOTOR = Motor("D")
-POWER_LIMIT = 80
-SPEED_LIMIT = 720
+LEFT_MOTOR = Motor("A")  # Left motor in Port A
+RIGHT_MOTOR = Motor("D") # Right motor in Port D
+POWER_LIMIT = 80         # Power limit = 80%
+SPEED_LIMIT = 720        # Speed limit = 720dps
+
 
 def wait_for_motor(motor: Motor):
-    while motor.get_speed() == 0:
+    "Function to block until motor completion"
+    while motor.get_speed() == 0:    # Wait for motor to spin up   (start)
         time.sleep(MOTOR_POLL_DELAY)
-    while motor.get_speed() != 0:
+    while motor.get_speed() != 0:    # Wait for motor to spin down (stop)
         time.sleep(MOTOR_POLL_DELAY)
 
-def init_motor(motor:Motor):
+
+def init_motor(motor: Motor):
+    "Function to initialize a motor"
     try:
-        motor.reset_encoder()
-        motor.set_limits(POWER_LIMIT, SPEED_LIMIT)
-        motor.set_power(0)
+        motor.reset_encoder()                      # Reset encoder to 0 value
+        motor.set_limits(POWER_LIMIT, SPEED_LIMIT) # Set power and speed limits
+        motor.set_power(0)                         # Stop the motor as well
     except IOError as error:
         print(error)
 
-def move_dist(distance, speed): # meters, dps
+
+def move_dist_fwd(distance, speed):  # meters, dps
+    "Function to move forward by user-specified distance and speed"
     try:
-        LEFT_MOTOR.set_dps(speed)
+        LEFT_MOTOR.set_dps(speed)                                      # Set speeds of motors
         RIGHT_MOTOR.set_dps(speed)
-        LEFT_MOTOR.set_position_relative(int(distance * DIST_TO_DEG))
+        LEFT_MOTOR.set_position_relative(int(distance * DIST_TO_DEG))  # Rotate wheels
         RIGHT_MOTOR.set_position_relative(int(distance * DIST_TO_DEG))
-        
+
         wait_for_motor(RIGHT_MOTOR)
     except IOError as error:
         print(error)
+
 
 def rotate_bot(angle, speed):
+    "Function to rotate in place by a user specified angle and speed"
     try:
-        LEFT_MOTOR.set_dps(speed)
+        LEFT_MOTOR.set_dps(speed)                                      # Set speeds
         RIGHT_MOTOR.set_dps(speed)
-        LEFT_MOTOR.set_position_relative(int(angle * ORIENT_TO_DEG))
-        RIGHT_MOTOR.set_position_relative(-int(angle * ORIENT_TO_DEG))
-        
+        LEFT_MOTOR.set_position_relative(int(angle * ORIENT_TO_DEG))   # Rotate L Wheel +ve
+        RIGHT_MOTOR.set_position_relative(-int(angle * ORIENT_TO_DEG)) # Rotate R Wheel -ve
+
         wait_for_motor(RIGHT_MOTOR)
     except IOError as error:
         print(error)
+
 
 def do_square(side_length):
     for i in range(4):
-        move_dist(side_length, FWD_SPEED)
-        rotate_bot(90, TRN_SPEED)
-    LEFT_MOTOR.set_power(0)
+        move_dist_fwd(side_length, FWD_SPEED) # Do side of square
+        rotate_bot(90, TRN_SPEED)             # Rotate 90 degrees
+    LEFT_MOTOR.set_power(0)                   # Motors off, square done
     RIGHT_MOTOR.set_power(0)
 
+
 try:
-    print('Square Driving Demo')
-    init_motor(LEFT_MOTOR)
-    init_motor(RIGHT_MOTOR)
+    print('Square Driving Demo') # Banner
+    init_motor(LEFT_MOTOR)       # Initialize L Motor
+    init_motor(RIGHT_MOTOR)      # Initialize R Motor
+
+    # Prompt for drive loop
     while True:
-        side_length = SQUARE_LENGTH
+        side_length = SQUARE_LENGTH # Assume default side length
         resp = input('Override default side length 0.5m? y/n')
         if resp.lower() == 'y':
             side_length = int(input('Enter square side length (m): '))
-        print('Starting sqaure driver with side length = {:0.2f}m'.format(side_length))
-        do_square(side_length)
+        print('Starting sqaure driver with side length = {:0.2f}m'.format(
+            side_length))
+        
+        do_square(side_length) # Drive in a square shape, with designated side_length
 
-except KeyboardInterrupt:
+except KeyboardInterrupt: # Abort program using ^C (Ctrl+C)
     BP.reset_all()
-
