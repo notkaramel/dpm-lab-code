3fed14da35bae7a7cad13fc83b112c6920099cdd
Ryan Au <auryan898@gmail.com>
2022-09-18 03:34:30 -0400

    Speed up the wheels for first example

--- lecture_07_1_Walkthrough.py
+++ lecture_07_1_Walkthrough.py
@@ -11,7 +11,7 @@ Date: January 13th, 2022
 from utils.brick import BP, TouchSensor, Motor, wait_ready_sensors, SensorError
 import time
 
-FORWARD_SPEED = 10          # speed constant = 30% power
+FORWARD_SPEED = 30          # speed constant = 30% power
 SENSOR_POLL_SLEEP = 0.05    # Polling rate = 50 msec
 
 T_SENSOR = TouchSensor(1)   # Touch Sensor in Port S1
