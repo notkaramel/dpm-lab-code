c62c195414578b57e74b3f1f0f4943d7d1319a6e
Ryan Au <auryan898@gmail.com>
2022-10-25 08:31:31 -0400

    Added different wait moving logic

--- tutorial_4_code.py
+++ tutorial_4_code.py
@@ -54,12 +54,15 @@ if __name__ == '__main__':
     try:
         motor.set_limits(dps=90)
         motor.set_position_relative(600)
+        motor.wait_is_moving()
         motor.wait_is_stopped()
         motor.reset_encoder()
         for pos in positions:
             motor.set_position(pos)
+            motor.wait_is_moving()
             motor.wait_is_stopped()
         motor.set_position(0)
+        motor.wait_is_moving()
         motor.wait_is_stopped()
 
         while True:
