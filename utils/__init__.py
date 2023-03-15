fb0b1021c07339f261eaac4af04897c37b64c959
Ryan Au <auryan898@gmail.com>
2023-03-15 04:35:31 -0400

    Update utils main script

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,9 +4,10 @@ import sys
 python = sys.executable
 packages_names = ' '.join(['simpleaudio', 'pygame'])
 
+
 def setup(debug=False):
     """Installs packages on the BrickPi where we expect them to be.
-    
+
     either 
     ./utils/lib
     or
@@ -17,7 +18,7 @@ def setup(debug=False):
     code = 0
 
     if os.path.basename(os.getcwd()) == 'utils':
-        os.chdir('..') # go to utils directory parent
+        os.chdir('..')  # go to utils directory parent
 
     cwd = os.getcwd()
 
@@ -25,9 +26,9 @@ def setup(debug=False):
         os.chdir(os.path.join('utils', 'lib'))
     elif 'lib' in os.listdir():
         os.chdir(os.path.join('lib'))
-    
+
     packages = ' '.join(os.listdir())
-    
+
     print(f"+++ From directory {os.getcwd()}")
     print(f"+++ Attempting install of:\n{packages}")
 
@@ -35,12 +36,13 @@ def setup(debug=False):
         code = os.system(f'{python} -m pip install {packages}')
 
     if code == 0:
-        return
+        return code
 
     # In case of failure of other things, try internet-based install
     print('+++ Local Installation Failed. Attempting Install from the Internet.')
-    code = os.system(f'{python} -m pip install {packages_names}')
+    if not debug:
+        code = os.system(f'{python} -m pip install {packages_names}')
 
     if code != 0:
         print("+++ ERROR: installation of extra python packages has completely failed")
-
+    return code
--- utils/__main__.py
+++ utils/__main__.py
@@ -3,13 +3,35 @@ from .__init__ import setup
 import sys
 
 debug = False
+help_msg = """
+Running "python3 -m utils" allows you to perform some actions
+-------------------------------------------------------------
+"python3 -m utils wifi"  - Starts a program to edit your robot's wifi networks
 
-if len(sys.argv) == 1:
-    exit()
+"python3 -m utils setup" - Starts a program to install Python Packages in nearby lib/ folders
+                           valid lib folders are relative to the utils folder:
+                           - utils/lib
+                           - utils/../lib
+
+"python3 -m utils setup true" - Only lists the packages it would install. Doesn't install.
+
+"""
+
 if len(sys.argv) > 2:
     debug = bool(sys.argv[2])
 
-if sys.argv[1] == "wifi":
+if len(sys.argv) == 1:
+    print(help_msg)
+elif sys.argv[1] == "wifi":
     wifi.main()
 elif sys.argv[1] == "setup":
-    setup(debug)
+    print()
+    code = setup(debug)
+    if code == 0:
+        if debug:
+            print("+++ Installation Would Be Successful")
+        else:
+            print("+++ Installation Was Successful")
+    print()
+else:
+    print(help_msg)
